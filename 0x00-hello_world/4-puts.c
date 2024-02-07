33e39c5b2e3cfa8823322530e869290ff2da0518
Abeer-M-Ali <abeerragab5211@gmail.com>
2024-02-07 22:25:27 +0200

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point To Start The Program
+ *
+ * always return 0
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
+
+return (0);
+}
